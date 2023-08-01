ae4825a4f146fff421994d52ea90ac8435be53b9
Sadsuk3 <108662874+Sadsuk3@users.noreply.github.com>
2023-08-01 18:04:04 +0200

    Add files via upload

---
+++ source/resource.h
@@ -0,0 +1,18 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Valostatus.rc
+//
+#define IDI_ICON1                       106
+#define IDI_ICON2                       106
+#define IDI_ICON3                       108
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
